a7482efdf2e7600e9ad29b58cac0a7c5dcbc13e5
ekvlbo <ekaterina.bosalkina@mail.ru>
2023-02-02 11:18:18 +0300

    init: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
+    let number = String(n);
+    let result = '';
   
+    
+       
+    if (number[0] === '-') {
+      for (let i = 1; i < number.length; i++) { 
+      result = number[i] + result; 
+      } 
+    } else {
+      for (let i = 0; i < number.length; i++) { 
+      result = number[i] + result; }  
+    
+    }
+    return result;
+    
 }
